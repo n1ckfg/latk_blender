4a380917db182725beb54f7b21824dc2577aead6
Nick Fox-Gieg <nick@fox-gieg.com>
2016-10-14 19:19:21 -0400

    caps added to final mesh util

--- util/mesh_final.py
+++ util/mesh_final.py
@@ -1,7 +1,7 @@
 from latk import *
 #~
 #name = getFileName() + "_render_final.blend"
-gpMesh(_bakeMesh=True, _joinMesh=True, _thickness=0.015, _saveLayers=True)
+gpMesh(_bakeMesh=True, _joinMesh=True, _thickness=0.02, _saveLayers=True, _caps=True)
 #~
 #bpy.ops.wm.save_as_mainfile(filepath="C:\\tmp\\render_test_final.blend")
 #bpy.ops.wm.save_as_mainfile(filepath="~/Desktop/" + name)
