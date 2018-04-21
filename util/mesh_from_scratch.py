ae1de3e01000a6a69743b51e8b01ff08b6f97919
n1ckfg <nick@fox-gieg.com>
2018-04-21 19:14:57 -0400

    fix batch import

--- util/mesh_from_scratch.py
+++ util/mesh_from_scratch.py
@@ -1,10 +1,18 @@
 import sys
+import bpy
 from latk import *
 #~
 argv = sys.argv
 argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
+bpy.context.scene.frame_start = 0
 readBrushStrokes(argv[0])
 splf()
+
+getName1 = argv[0].split("/")
+getName2 = getName1[len(getName1)-1].split("\\")
+getName3 = getName2[len(getName2)-1].split(".")[0]
+
+saveFile(getName3)
 gpMesh(_resolution=1, _decimate=0.02, _thickness=0.02, _saveLayers=True, _usePressure=True)
 #~
