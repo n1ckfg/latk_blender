a40c577a2e1643da0429027896aa824da94831d7
Nick Fox-Gieg <nick@fox-gieg.com>
2018-12-28 00:44:16 -0600

    Update mesh_settings.py

--- util/mesh_settings/mesh_settings.py
+++ util/mesh_settings/mesh_settings.py
@@ -1,4 +1,4 @@
-from latk import *
+from latk_blender import *
 #~
 latk_settings=bpy.context.scene.latk_settings
 gpMesh(_saveLayers=True, _thickness=latk_settings.thickness, _remesh=latk_settings.remesh_mode.lower(), _resolution=latk_settings.resolution, _bevelResolution=latk_settings.bevelResolution, _decimate=latk_settings.decimate, _vertexColorName=latk_settings.vertexColorName)
