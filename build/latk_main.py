402e08670155d83a79b0a43d41f0826fbe160b6d
Nick Fox-Gieg <nick@fox-gieg.com>
2018-12-28 23:59:54 -0600

    refactor

--- build/latk_main.py
+++ build/latk_main.py
@@ -1,32 +1,3 @@
-'''
-LIGHTNING ARTIST TOOLKIT v1.0.0
-
-The Lightning Artist Toolkit was developed with support from:
-   Canada Council on the Arts
-   Eyebeam Art + Technology Center
-   Ontario Arts Council
-   Toronto Arts Council
-   
-Copyright (c) 2018 Nick Fox-Gieg
-http://fox-gieg.com
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-The Lightning Artist Toolkit (Blender) is free software: you can redistribute it 
-and/or modify it under the terms of the GNU General Public License 
-as published by the Free Software Foundation, either version 3 of 
-the License, or (at your option) any later version.
-
-The Lightning Artist Toolkit (Blender) is distributed in the hope that it will 
-be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
-of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with the Lightning Artist Toolkit (Blender).  If not, see 
-<http://www.gnu.org/licenses/>.
-'''
-
 # 1 of 10. MAIN
 
 bl_info = {
