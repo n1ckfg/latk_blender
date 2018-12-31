562d927b9c6595608491463c90ee03cd0d8364be
Nick Fox-Gieg <nick@fox-gieg.com>
2018-12-31 16:58:04 -0500

    Update alembic_from_scratch.py

--- util/alembic_from_scratch/alembic_from_scratch.py
+++ util/alembic_from_scratch/alembic_from_scratch.py
@@ -15,7 +15,8 @@ name3 = name2[len(name2)-1].split(".")[0]
 
 saveFile(name3)
 latk_settings=bpy.context.scene.latk_settings
-gpMesh(_saveLayers=True, _thickness=latk_settings.thickness, _remesh=latk_settings.remesh_mode.lower(), _resolution=latk_settings.resolution, _bevelResolution=latk_settings.bevelResolution, _decimate=latk_settings.decimate, _vertexColorName=latk_settings.vertexColorName)
+la = fromGpToLatk()
+gpMesh(_la=la, _saveLayers=True, _thickness=latk_settings.thickness, _remesh=latk_settings.remesh_mode.lower(), _resolution=latk_settings.resolution, _bevelResolution=latk_settings.bevelResolution, _decimate=latk_settings.decimate, _vertexColorName=latk_settings.vertexColorName)
 
 name4 = name3 + "_ASSEMBLY"
 
