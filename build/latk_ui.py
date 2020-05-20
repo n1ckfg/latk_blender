40fcd27c0dc4d30b0e0769563c1374dd04446829
Nick Fox-Gieg <nick@fox-gieg.com>
2020-05-20 01:03:32 -0400

    update menu for 2.8

--- build/latk_ui.py
+++ build/latk_ui.py
@@ -1431,36 +1431,34 @@ classes = (
 )
 
 def register():
-    '''
-    bpy.utils.register_module(__name__)
+    #bpy.utils.register_module(__name__)
+    for cls in classes:
+        bpy.utils.register_class(cls)   
 
     bpy.types.Scene.latk_settings = PointerProperty(type=LatkProperties)
-    bpy.types.INFO_MT_file_import.append(menu_func_import)
-    bpy.types.INFO_MT_file_export.append(menu_func_export)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
     bpy.types.Scene.freestyle_gpencil_export = PointerProperty(type=FreestyleGPencil)
     
     parameter_editor.callbacks_lineset_pre.append(export_fill)
     parameter_editor.callbacks_lineset_post.append(export_stroke)
-    '''
-    for cls in classes:
-        bpy.utils.register_class(cls)   
 
 def unregister():
-    '''
-    bpy.utils.unregister_module(__name__)
+    #bpy.utils.unregister_module(__name__)
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
 
     del bpy.types.Scene.latk_settings
-    bpy.types.INFO_MT_file_import.remove(menu_func_import)
-    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
     del bpy.types.Scene.freestyle_gpencil_export
     
     parameter_editor.callbacks_lineset_pre.remove(export_fill)
     parameter_editor.callbacks_lineset_post.remove(export_stroke)
-    '''
-    for cls in reversed(classes):
-        bpy.utils.unregister_class(cls)
+
+
 
 if __name__ == "__main__":
     register()
