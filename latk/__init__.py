7b603e6bac8832bfb3cd0c0920661d901ddf63f5
Nick Fox-Gieg <nick@fox-gieg.com>
2023-11-05 09:10:42 -0500

    Update __init__.py

--- latk/__init__.py
+++ latk/__init__.py
@@ -1,35 +1,35 @@
-from latk.latk_main import Latk
-from latk.latk_main import LatkLayer
-from latk.latk_main import LatkFrame
-from latk.latk_main import LatkStroke
-from latk.latk_main import LatkPoint
+from . latk_main import Latk
+from . latk_main import LatkLayer
+from . latk_main import LatkFrame
+from . latk_main import LatkStroke
+from . latk_main import LatkPoint
 
-from latk.latk_tilt import memoized_property
-from latk.latk_tilt import binfile
-from latk.latk_tilt import BadTilt
-from latk.latk_tilt import BadMetadata
-from latk.latk_tilt import MissingKey
-from latk.latk_tilt import validate_metadata
-from latk.latk_tilt import Tilt
-from latk.latk_tilt import _make_ext_reader
-from latk.latk_tilt import _make_stroke_ext_reader
-from latk.latk_tilt import _make_cp_ext_reader
-from latk.latk_tilt import Sketch
-from latk.latk_tilt import Stroke
-from latk.latk_tilt import ControlPoint
-from latk.latk_tilt import tiltBrushJson_Grouper
-from latk.latk_tilt import tiltBrushJson_DecodeData
+from . latk_tilt import memoized_property
+from . latk_tilt import binfile
+from . latk_tilt import BadTilt
+from . latk_tilt import BadMetadata
+from . latk_tilt import MissingKey
+from . latk_tilt import validate_metadata
+from . latk_tilt import Tilt
+from . latk_tilt import _make_ext_reader
+from . latk_tilt import _make_stroke_ext_reader
+from . latk_tilt import _make_cp_ext_reader
+from . latk_tilt import Sketch
+from . latk_tilt import Stroke
+from . latk_tilt import ControlPoint
+from . latk_tilt import tiltBrushJson_Grouper
+from . latk_tilt import tiltBrushJson_DecodeData
 
-from latk.latk_kmeans import kdist
-from latk.latk_kmeans import KMeans
-from latk.latk_kmeans import KCentroid
-from latk.latk_kmeans import KParticle
-from latk.latk_kmeans import KCluster
+from . latk_kmeans import kdist
+from . latk_kmeans import KMeans
+from . latk_kmeans import KCentroid
+from . latk_kmeans import KParticle
+from . latk_kmeans import KCluster
 
-from latk.latk_rdp import pldist
-from latk.latk_rdp import rdp_rec
-from latk.latk_rdp import _rdp_iter
-from latk.latk_rdp import rdp_iter
-from latk.latk_rdp import rdp
+from . latk_rdp import pldist
+from . latk_rdp import rdp_rec
+from . latk_rdp import _rdp_iter
+from . latk_rdp import rdp_iter
+from . latk_rdp import rdp
 
-from latk.latk_zip import InMemoryZip
+from . latk_zip import InMemoryZip
