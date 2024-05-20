bf7bb5b6653828631aa811970c5f7a0be9230d47
Nick Fox-Gieg <nick@fox-gieg.com>
2024-05-20 17:52:04 -0400

    compile swig python 3.11

--- skeleton_tracing/swig/compile.bat
+++ skeleton_tracing/swig/compile.bat
@@ -12,8 +12,8 @@ rem WINDOWS
 rem https://github.com/LingDong-/skeleton-tracing/issues/14
 rem gcc -fPIC -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I/$(python3-config --cflags)
 rem g++ -shared $(python3-config --cflags --ldflags) *.o -o _trace_skeleton.so
-gcc -fPIC -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\include
-g++ -shared *.o -o _trace_skeleton.pyd -L%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\libs -lpython310
+gcc -fPIC -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python311\include
+g++ -shared *.o -o _trace_skeleton.pyd -L%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python311\libs -lpython311
 
 rem quick tests
 rem python3 -i -c "import trace_skeleton; trace_skeleton.trace('\0\0\0\1\1\1\0\0\0',3,3); print(trace_skeleton.len_polyline());"
