9e22a506f1a7c78c9d1b52595dfc48391d5d228e
Nick Fox-Gieg <nick@fox-gieg.com>
2024-05-21 11:33:33 -0400

    shell win swig tools

--- skeleton_tracing/swig/windows-get-gcc.bat
+++ skeleton_tracing/swig/windows-get-gcc.bat
@@ -1,11 +1,18 @@
 @echo off
 
 cd %~dp0
-powershell -Command "Invoke-WebRequest https://github.com/skeeto/w64devkit/releases/download/v1.20.0/w64devkit-1.20.0.zip -OutFile w64devkit.zip"
+powershell -Command "Invoke-WebRequest https://fox-gieg.com/patches/github/n1ckfg/latk_blender/skeleton_tracing/swig/swig.zip -OutFile swig.zip"
+powershell Expand-Archive swig.zip -DestinationPath .
+del swig.zip
+
+powershell -Command "Invoke-WebRequest https://fox-gieg.com/patches/github/n1ckfg/latk_blender/skeleton_tracing/swig/w64devkit.zip -OutFile w64devkit.zip"
 powershell Expand-Archive w64devkit.zip -DestinationPath .
 del w64devkit.zip
 
-echo Move w64devkit to desired location and add bin directory as environment variable.
+echo Move w64devkit and swig to desired location.
+echo Add the following as environment variables:
+echo w64devkit\bin\
+echo swig\
 
 @pause
 
