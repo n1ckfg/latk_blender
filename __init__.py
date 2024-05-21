9669847842ee0f98018fac8482f8d9e3a99c0c25
Nick Fox-Gieg <nick@fox-gieg.com>
2024-05-21 17:18:53 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -700,7 +700,7 @@ class Latk_Button_InstallPytorch(bpy.types.Operator):
             # MPS support is still in development, and so needs a pre-release version
             runCmd([python_exe, '-m', 'pip', 'install', '--pre', '--upgrade', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/nightly/cpu'])
         elif (whichPlatform == "windows"):
-            runCmd([python_exe, '-m', 'pip', 'install', '--upgrade', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/torch_stable.html'])
+            runCmd([python_exe, '-m', 'pip', 'install', '--upgrade', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/cu118'])
         else:
             runCmd([python_exe, '-m', 'pip', 'install', '--upgrade', 'torch', 'torchvision', 'torchaudio']) #, '--index-url', 'https://download.pytorch.org/whl/torch_stable.html'])
 
